e43705daa73d93481b59496b37765ae43434a83e
shuhai-miao <shuhai.miao@NYSHEX.com>
2023-05-06 15:23:47 -0400

    Change refactoring WorldMap.toString

--- demo/src/main/java/com/example/WorldMap.java
+++ demo/src/main/java/com/example/WorldMap.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class WorldMap {
 
   private final World world;
@@ -14,13 +17,11 @@ public class WorldMap {
 
   @Override
   public String toString() {
-    StringBuilder sb = new StringBuilder();
-    for (int y = 0; y < 10; y++) {
-      for (int x = 0; x < 10; x++) {
-        sb.append(world.isAlive(x, y) ? "*" : ".");
-      }
-      sb.append("\n");
-    }
-    return sb.toString();
-  } 
+    return IntStream.range(0, 10)
+        .mapToObj(y -> IntStream.range(0, 10)
+            .mapToObj(x -> world.isAlive(x, y) ? "*" : ".")
+            .collect(Collectors.joining()))
+        .collect(Collectors.joining("\n"));
+  }
+
 }
