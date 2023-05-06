984d9ed31b554f48b0fcaca14a68a85ee6efb1aa
shuhai-miao <shuhai.miao@NYSHEX.com>
2023-05-06 07:08:39 -0400

    Add: a_new_world_with_no_cells_is_empty

---
+++ demo/src/main/java/com/example/World.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class World {
+
+  public boolean isEmpty() {
+    return true;
+  }
+
+}
--- demo/src/test/java/com/example/GameOfLifeTest.java
+++ demo/src/test/java/com/example/GameOfLifeTest.java
@@ -1,5 +1,18 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
 public class GameOfLifeTest {
+
+  @Test
+  public void a_new_world_with_no_cells_is_empty() {
+
+    // Given a new world with no cells
+    World world = new World();
+
+    // When I ask if it is empty
+    // Then it should be empty
+    assert world.isEmpty();
+  }
 
 }
