2eab32511a33aa073bbbe713c2137994bf6dcf3d
shuhai-miao <shuhai.miao@NYSHEX.com>
2023-05-06 07:15:27 -0400

    Add a_living_cell_can_be_added_to_the_world

--- demo/src/main/java/com/example/World.java
+++ demo/src/main/java/com/example/World.java
@@ -2,11 +2,17 @@ package com.example;
 
 public class World {
 
+  private Cell cell;
+
   public boolean isEmpty() {
-    return true;
+    return cell == null;
   }
 
   public void tick() {
+  }
+
+  public void add(Cell cell) {
+    this.cell = cell;
   }
 
 }
--- demo/src/test/java/com/example/GameOfLifeTest.java
+++ demo/src/test/java/com/example/GameOfLifeTest.java
@@ -4,6 +4,20 @@ import org.junit.jupiter.api.Test;
 
 public class GameOfLifeTest {
 
+
+  @Test
+  public void a_living_cell_can_be_added_to_the_world() {
+
+    // Given a world
+    World world = new World();
+
+    // When I add a living cell to the world
+    world.add(new Cell());
+
+    // Then the world is not empty
+    assert !world.isEmpty();
+  }
+
   @Test
   public void An_empty_world_stays_empty() {
 
